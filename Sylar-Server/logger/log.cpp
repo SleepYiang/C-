fe83ef1dee4635951550c4767f23ba0655c081bd
sleepyiang <yiang_1970@163.com>
2025-08-15 19:19:11 +0800

    相关的项目

---
+++ Sylar-Server/logger/log.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adrotheall on 2025/8/15.
+//
+
+#include "log.h"
---
+++ Sylar-Server/logger/log.h
@@ -0,0 +1,19 @@
+#ifndef SYLAR_SERVER_LOG_H
+#define SYLAR_SERVER_LOG_H
+#include<string>
+#include<stdint.h>
+#include<memory>
+#include<list>
+#include<sstream>
+#include<fstream>
+#include<map>
+#include<stdarg.h>
+#include<vector>
+
+class log
+{
+
+};
+
+
+#endif //SYLAR_SERVER_LOG_H
