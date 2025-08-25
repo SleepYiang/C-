0579bd21074b5f65e5f2a36152b486e1853bcc60
sleepyiang <yiang_1970@163.com>
2025-08-26 07:58:25 +0800

    base相关笔记

--- Base/note/operator.cpp
+++ Base/note/operator.cpp
@@ -39,6 +39,16 @@ public:
         strcpy(pstr_, str.pstr_);
         return *this;
     }
+    //给字符串类型提供迭代器
+    class iterator
+    {
+    public:
+    private:
+        char * p_;
+    };
+    //begin返回容器底层元素的迭代器的标志
+    iterator begin(){return iterator(pstr_);    }
+    iterator end();
 private:
     char*pstr_;
     friend ostream& operator<<(ostream &out,const String &str);
@@ -55,12 +65,31 @@ ostream& operator<<(ostream &out,const String &str)
 {
     out<<str.pstr_<<endl;
 }
+//泛型算法参数接受的都是迭代器
 int main()
 {
-    String str1;
-    String str2="aaa";
-    String str3="bbb";
-    String str4=str3+str2;
-    cout<<str4;
-    return 0;
+    string str1="hello world";//str1
+    //容器的迭代器
+    string::iterator it=str1.begin();//返回首元素的地址
+    for (;it!=str1.end();it++)
+    {
+        cout<<*it<<endl;//访问容器内部元素的值
+    }
+    // String str1;
+    // String str2="aaa";
+    // String str3="bbb";
+    // String str4=str3+str2;
+    // cout<<str4;
+    // return 0;
 }
+
+
+//new和delete
+//new和malloc的区别
+//1.malloc按字节开辟内存，new开辟内存的时候需要指定类型 new int[]
+    //maloc开辟内训的返回的是void* operator new->int*
+//2.malloc只负责开辟空间，new不仅要开辟空间还需要初始化
+//3.malloc开辟失败返回nullptr，new则是抛出异常
+
+//free和delete的区别
+//1.delete :先调用析构再free
