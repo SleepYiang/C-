37eb652111f70e3a28541e9dd904ca8d7dadfc95
sleepyiang <yiang_1970@163.com>
2025-09-08 21:01:12 +0800

    基本架构（项目的基）

---
+++ Sylar-Server/main.cpp
@@ -0,0 +1,19 @@
+//日志系统
+
+//目录结构
+// bin  二进制的文件
+// build   中间件的路径
+//cmake    cmake的文件路径
+// 1) log类
+//    logger  定义日志类别
+//          formatter日志格式
+//    appender   日志的输出地方
+//协程库
+
+//socket函数库
+
+//http协议开发
+
+//分布式协议
+
+//推荐系统
