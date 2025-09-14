4ac0d04417a1989e583bb4e6f67a9d1820bc8e34
sleepyiang <yiang_1970@163.com>
2025-09-14 17:19:15 +0800

    GDB 相关调试

--- Linux/note/fileIO.cpp
+++ Linux/note/fileIO.cpp
@@ -8,10 +8,18 @@
     //程序（不占用内存空间，只占用磁盘空间）
     //进程（正在运行的程序，占用内存空间）
 
+//文件描述符
+    //文件描述符在内核区（pcb 进程控制块）（内核就是一个程序）
+    //文件描述符表（数组）{一个进程可以打开多个文件}
+    //前三个是被输入输出错误描述符占用（一个文件可以有多个文件描述符）
 
 
-//
-//
-/
+//open打开文件
+    // int open(const char* pathname,int flags);
+    //int open(const char* pathname,int flags,mode_t mode);
+    //int close(int fd);
+    //ssize_t read(int fd,void *buf,size_t count);
+    //ssize_t write(int fd,void *buf,size_t count);
+    //off_t lseek(int fd,void *buf,size_t count);
 
-    /
+    // umsask
